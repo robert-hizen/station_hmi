fc8ee4fb56dc80ae42a3be01a5a07d1d63a64fdf
mehdi <hizenberg917@gmail.com>
2025-07-08 18:27:04 +0330

    finsished station number

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from config import config
 from src import bg
+from src import st_number
 import logging
 if __name__ == '__main__':
     conf = config.Configuration()
     back_fround_color = bg.Bg(conf)
+    station_number = st_number.StationNumber(conf)
     try:
         # configure our display
         conf.init_display()
         back_fround_color.background_color()
+        station_number.st_number(9)
         conf.show_image()
     except Exception as e:
         logging.error(f"Error : {e}")
---
+++ src/st_number.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+from PIL import Image , ImageDraw , ImageFont
+sys.path.append('..')
+from config import config
+class StationNumber:
+    def __init__(self , conf : config.Configuration):
+        self.config = conf
+    def st_number(self , num):
+        if not (1<= num <= 9):
+            logging.warning("Invalid station number")
+        try:
+            # canvas = Image.new("RGB", (240, 240), "#2C3E50")  # یا "#000000" برای مشکی
+            img = Image.open(f'template/picture/{num}-solid.jpg').convert("RGB")
+            self.config.image.paste(img , (120,8))
+        except Exception as e:
+            logging.error(f"Error loading image: {e}")
+          
