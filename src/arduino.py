0b76c3d10e506cc0f5b960fe830bf4390acc7def
mehdisogandi <hizenberg917@gmail.com>
2025-07-12 13:55:33 +0330

    finished connection cam , netwrok , arduino

--- src/arduino.py
+++ src/arduino.py
@@ -1,27 +1,34 @@
-import sys
 import time
-import sys
+import threading
+import logging
 from PIL import Image
-sys.path.append('..')
 from config import config
+
 class Arduino:
-    X_CORDINATE = 100
-    Y_CORDIANATE = 165
-    def __init__(self ,conf : config.Configuration , status = None):
+    X_CORDINATE = 155
+    Y_CORDINATE = 165
+
+    def __init__(self, conf: config.Configuration, status=None):
         self.config = conf
-        self.arduino_state = status
+        self.status = status  # None یا 'on'
         self.dir = '/home/user-null/Documents/station_lcd/template/picture/arduino'
-    def arduino_connection(self):
-        img = Image.open(f'{self.dir}/arduino.jpg')
-        if self.arduino_state is None:
-            self.config.image.paste(img , (Arduino.X_CORDINATE , Arduino.Y_CORDIANATE))
+        self.last_blink_time = time.time()
+        self.blink_state = False
 
-            # while True:
-            #     self.config.disp.ShowImage(self.config.image.rotate(180))
-            #     time.sleep(0.5)
+        self.img_on = Image.open(f'{self.dir}/arduino.jpg').convert("RGBA")
+        self.clear_img = Image.new("RGBA", self.img_on.size, (0, 0, 0, 0))
+        self.current_image = self.clear_img.copy()
 
-            #     clear_img = Image.new("RGB" , (img.width , img.height) , '#f7f1e3')
-            #     self.config.image.paste(clear_img , (Arduino.X_CORDINATE , Arduino.Y_CORDIANATE))
-            #     self.config.disp.ShowImage(self.config.image.rotate(180))
-            #     time.sleep(0.5)
-                # self.config.disp.ShowImage(self)
+    def update(self):
+        now = time.time()
+        if self.status is None:
+            if now - self.last_blink_time > 0.5:
+                self.blink_state = not self.blink_state
+                self.last_blink_time = now
+            self.current_image = self.img_on if self.blink_state else self.clear_img
+        elif self.status == 'on':
+            self.current_image = self.img_on
+
+    def update_status(self, new_status):
+        if new_status != self.status:
+            self.status = new_status
--- src/network.py
+++ src/network.py
@@ -1,33 +1,37 @@
-import sys
-import logging
 import time
+import threading
+import logging
 from PIL import Image
-sys.path.append('..')
 from config import config
+
 class Network:
     X_CORDINATE = 95
     Y_CORDINATE = 165
-    def __init__(self ,conf : config.Configuration , connection_status = None):
-        self.config =  conf
-        self.conn_status = connection_status
-        self.dir  = '/home/user-null/Documents/station_lcd/template/picture/network'
-    def connection(self):
-        try:
-            try: 
-                if self.conn_status is None:
-                    while True:
-                        img = Image.open(f'{self.dir}/no_con.jpg')
-                        self.config.image.paste(img, (Network.X_CORDINATE,Network.Y_CORDINATE))
-                        self.config.disp.ShowImage(self.config.image.rotate(180))
-                        time.sleep(0.5)
 
-                        clear_img = Image.new("RGB" , (img.width , img.height) , '#f7f1e3')
-                        self.config.image.paste(clear_img , (Network.X_CORDINATE , Network.Y_CORDINATE))
-                        self.config.disp.ShowImage(self.config.image.rotate(180))
-                        time.sleep(0.5)
-                elif self.conn_status == 'weak':
-                    img = Image.open(f'{self.dir}/weak_con.jpg')
-            except ValueError as e:
-                pass
-        except Exception as e:
-            logging.error(f'Unable to load image : {e}')
+    def __init__(self, conf: config.Configuration, status=None):
+        self.config = conf
+        self.status = status  # None یا 'best'
+        self.dir = '/home/user-null/Documents/station_lcd/template/picture/network'
+        self.last_blink_time = time.time()
+        self.blink_state = False
+
+        self.img_best = Image.open(f'{self.dir}/best_con.jpg').convert("RGBA")
+        self.img_no_con = Image.open(f'{self.dir}/no_con.jpg').convert("RGBA")
+        self.clear_img = Image.new("RGBA", self.img_no_con.size, (0, 0, 0, 0))
+        self.current_image = self.clear_img.copy()
+
+    def update(self):
+        now = time.time()
+        if self.status is None:
+            # چشمک زدن برای حالت no connection
+            if now - self.last_blink_time > 0.5:
+                self.blink_state = not self.blink_state
+                self.last_blink_time = now
+            self.current_image = self.img_no_con if self.blink_state else self.clear_img
+        elif self.status == 'best':
+            # تصویر ثابت برای حالت best
+            self.current_image = self.img_best
+
+    def update_status(self, new_status):
+        if new_status != self.status:
+            self.status = new_status
