14f7121d082ff25d3c313b34eedf478a192cac01
mehdisogandi <hizenberg917@gmail.com>
2025-07-14 15:02:06 +0330

    finished status errors

--- src/status.py
+++ src/status.py
@@ -22,32 +22,31 @@ class Status:
         else :
             logging.error("Please enter valid status")
             self.config.module_die()
-    def circle(self):
-        self.config.draw.arc(
-            [(30,95) ,(100,150)],
-            start= 0,
-            end=360,
-            fill='#f7f1e3',
-            width=3  
-        )
+    # def circle(self):
+    #     self.config.draw.arc(
+    #         [(30,95) ,(100,150)],
+    #         start= 0,
+    #         end=360,
+    #         fill='#f7f1e3',
+    #         width=3  
+    #     )
     @staticmethod
-    def wrap_text(text , line_lenght = 13):
+    def wrap_text(text , line_lenght = 18):
         return '\n'.join([text[i:i+line_lenght] for i in range(0,len(text) , line_lenght)])
-    def Status_logo_message(self ,txt ,status , error_text):
-        font = self.config.bold_font
-        self.config.draw.text(
-            (42 ,128),
-            text= txt,
-            fill= '#4b4b4b',
-            font= font
-        )
+    def Status_logo_message(self ,error_text):
+        # self.config.draw.text(
+        #     (42 ,128),
+        #     text= txt,
+        #     fill= '#4b4b4b',
+        #     font= font
+        # )
         # error_img = Image.open(f'template/picture/warning_error/{status}.jpg')
         # self.config.image.paste(error_img , (55 , 100))
 
-        wrapped_text = self.wrap_text(error_text , line_lenght= 13 )
+        wrapped_text = self.wrap_text(error_text , line_lenght= 18 )
         error_font = self.config.big_bold_font
         self.config.draw.text(
-            (105 , 92),
+            (30 , 135),
             text=wrapped_text,
             fill= '#f7f1e3',
             font= error_font
