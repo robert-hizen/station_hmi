ff7df4e89bdc396f9048920b9580adcc255fcaa7
mehdisogandi <hizenberg917@gmail.com>
2025-07-16 16:09:02 +0330

    finished all feture

--- src/cycle_time.py
+++ src/cycle_time.py
@@ -12,25 +12,28 @@ class CycleTime:
         temp_img = Image.new("RGBA" , (120,120) , (0,0,0,0))
         temp_draw = ImageDraw.Draw(temp_img)
 
-        temp_draw.pieslice(
+        temp_draw.rectangle(
             [
-                (30 , 80),
-                (70 , 100)
+                (16 , 80),
+                (67, 100)
             ],
-            start=0,
-            end=360,
             fill='#aaa69d',
-            outline='#aaa69d'
 
         )
+        temp_draw.pieslice(
+            [(5,95),(70,120)],
+            start=180,
+            end=360,
+            fill=(0,0,0,0)
+        )
         temp_draw.text(
-            (40,85),
+            (20,82),
             text=f'{cycle_txt}',
             fill='#f5f6fa',
             font=self.font
         )
 
-        rotated_img = temp_img.rotate(45 , expand=True).convert("RGBA")
+        rotated_img = temp_img.rotate(62 , expand=True).convert("RGBA")
 
         y_top = -55
         self.config.image.paste(rotated_img , ( -70 , y_top) , rotated_img)
